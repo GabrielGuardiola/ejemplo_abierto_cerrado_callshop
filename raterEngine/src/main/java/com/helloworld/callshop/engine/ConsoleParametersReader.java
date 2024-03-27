637a4670010522f371c9fb5438486f028bf8859c
Gguardiola <gabriel.guardiola1999@gmail.com>
2024-03-27 09:49:01 +0100

    Refactored

--- raterEngine/src/main/java/com/helloworld/callshop/engine/ConsoleParametersReader.java
+++ raterEngine/src/main/java/com/helloworld/callshop/engine/ConsoleParametersReader.java
@@ -1,7 +1,5 @@
 package com.helloworld.callshop.engine;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.helloworld.callshop.rater.rate.factory.ParametersReader;
 import com.helloworld.callshop.rater.rate.factory.Parameter;
 import com.helloworld.callshop.rater.rate.factory.ParametersMapper;
@@ -29,24 +27,6 @@ public class ConsoleParametersReader implements ParametersReader {
             }
             paramsMapper.put(parameter.getName(), parameterValue);
         }
-
-        /*
-        ParametersMapperImpl paramsMapper = new ParametersMapperImpl();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(json);
-
-        for (Parameter parameter: parameters) {
-            String parameterValue = rootNode.get(parameter.getName()).asText();
-            while (!parameter.getValidator().test(parameterValue)) {
-                // You may want to handle invalid input within the JSON differently,
-                // such as throwing an exception or logging a message.
-                // For simplicity, this example prompts the user to enter valid input.
-                System.out.print("Invalid value for " + parameter.getDescription() + ". Please enter a valid value:");
-                parameterValue = consoleInput.nextLine();
-            }
-            paramsMapper.put(parameter.getName(), parameterValue);
-        }
-        */
 
         return paramsMapper;
     }
