289999a12e176f357573464012314fa170ef15ce
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-03-27 09:12:33 +0100

    feature started

--- raterEngine/src/main/java/com/helloworld/callshop/engine/JsonParametersReader.java
+++ raterEngine/src/main/java/com/helloworld/callshop/engine/JsonParametersReader.java
@@ -1,35 +1,14 @@
 package com.helloworld.callshop.engine;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.helloworld.callshop.rater.rate.factory.ParametersReader;
 import com.helloworld.callshop.rater.rate.factory.Parameter;
 import com.helloworld.callshop.rater.rate.factory.ParametersMapper;
+import com.helloworld.callshop.rater.rate.factory.ParametersReader;
 
 import java.util.List;
-import java.util.Scanner;
 
-public class ConsoleParametersReader implements ParametersReader {
+public class JsonParametersReader implements ParametersReader {
 
-
-
-    @Override
     public ParametersMapper readParameters(List<Parameter> parameters) {
-
-        ParametersMapperImpl paramsMapper = new ParametersMapperImpl();
-
-        Scanner consoleInput = new Scanner(System.in);
-
-        for (Parameter parameter: parameters) {
-            System.out.print(parameter.getDescription() + ":");
-            String parameterValue = consoleInput.nextLine();
-            while (!parameter.getValidator().test(parameterValue)) {
-                System.out.print("El valor introducido es incorrecto. Repita por favor" + ":");
-                parameterValue = consoleInput.nextLine();
-            }
-            paramsMapper.put(parameter.getName(), parameterValue);
-        }
-
         /*
         ParametersMapperImpl paramsMapper = new ParametersMapperImpl();
         ObjectMapper objectMapper = new ObjectMapper();
@@ -47,9 +26,6 @@ public class ConsoleParametersReader implements ParametersReader {
             paramsMapper.put(parameter.getName(), parameterValue);
         }
         */
-
-        return paramsMapper;
     }
-
 
 }
